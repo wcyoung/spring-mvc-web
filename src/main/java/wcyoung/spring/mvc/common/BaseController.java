d77d5c06bea7e43d725d0827fe24df1bbd8d0940
Chiyoung Won <wcyoung98@gmail.com>
2019-11-25 21:27:04 +0900

    Base Class 추가 (Abstract)

---
+++ src/main/java/wcyoung/spring/mvc/common/BaseController.java
@@ -0,0 +1,8 @@
+package wcyoung.spring.mvc.common;
+
+/**
+ * TODO Move to Common
+ */
+public class BaseController extends CommonBase {
+
+}
---
+++ src/main/java/wcyoung/spring/mvc/common/CommonBase.java
@@ -0,0 +1,13 @@
+package wcyoung.spring.mvc.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO Move to Common
+ */
+public abstract class CommonBase {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+}
