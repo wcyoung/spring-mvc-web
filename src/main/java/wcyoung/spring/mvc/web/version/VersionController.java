4c366651526aac3884b9c82191cee21674331531
Chiyoung Won <wcyoung98@gmail.com>
2019-11-25 21:07:26 +0900

    Version 조회하는 API 추가

---
+++ src/main/java/wcyoung/spring/mvc/web/version/VersionController.java
@@ -0,0 +1,38 @@
+package wcyoung.spring.mvc.web.version;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/version")
+@RestController
+public class VersionController {
+
+    @Resource
+    private ResourceLoader resourceLoader;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getVersionInfo() {
+        Map<String, Object> body = new HashMap<>();
+
+        try {
+            Properties properties = new Properties();
+            properties.load(resourceLoader.getResource("/META-INF/MANIFEST.MF").getInputStream());
+            body.put("version", properties.getProperty("Implementation-Version"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Map<String,Object>>(body, HttpStatus.OK);
+    }
+
+}
