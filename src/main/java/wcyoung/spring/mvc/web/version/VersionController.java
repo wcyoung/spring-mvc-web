97534488814becbaa46188793f1945a491ae3f00
Chiyoung Won <wcyoung98@gmail.com>
2019-11-26 23:47:58 +0900

    VersionService 추가

--- src/main/java/wcyoung/spring/mvc/web/version/VersionController.java
+++ src/main/java/wcyoung/spring/mvc/web/version/VersionController.java
@@ -2,12 +2,10 @@ package wcyoung.spring.mvc.web.version;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,16 +19,14 @@ import wcyoung.spring.mvc.common.BaseController;
 public class VersionController extends BaseController {
 
     @Resource
-    private ResourceLoader resourceLoader;
+    private VersionService versionService;
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> getVersionInfo() {
         Map<String, Object> body = new HashMap<>();
 
         try {
-            Properties properties = new Properties();
-            properties.load(resourceLoader.getResource("/META-INF/MANIFEST.MF").getInputStream());
-            body.put("version", properties.getProperty("Implementation-Version"));
+            body.put("version", versionService.getVersionInfo());
         } catch (Exception e) {
             log.error("Exception : {}", ExceptionUtils.getStackTrace(e));
             return new ResponseEntity<Map<String,Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
---
+++ src/main/java/wcyoung/spring/mvc/web/version/VersionService.java
@@ -0,0 +1,25 @@
+package wcyoung.spring.mvc.web.version;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import wcyoung.spring.mvc.common.BaseService;
+
+@Service
+public class VersionService extends BaseService {
+
+    @Resource
+    private ResourceLoader resourceLoader;
+
+    public String getVersionInfo() throws IOException {
+        Properties properties = new Properties();
+        properties.load(resourceLoader.getResource("/META-INF/MANIFEST.MF").getInputStream());
+        return properties.getProperty("Implementation-Version");
+    }
+
+}
