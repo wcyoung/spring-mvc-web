319b8ab0ed6eb16d1c17d273b61a56751a1f0f25
Chiyoung Won <wcyoung98@gmail.com>
2019-12-04 23:31:23 +0900

    Service running time logging AOP 추가

---
+++ src/main/java/wcyoung/spring/mvc/common/aop/LoggingAspect.java
@@ -0,0 +1,32 @@
+package wcyoung.spring.mvc.common.aop;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Around(value = "execution(* wcyoung.spring.mvc.web..*Service.*(..))")
+    public Object logMethodRunningTime(ProceedingJoinPoint pjp) throws Throwable {
+        String className = pjp.getSignature().getDeclaringTypeName();
+        String methodName = pjp.getSignature().getName();
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        Object returnValue = pjp.proceed();
+        stopWatch.stop();
+
+        long runningTime = stopWatch.getTime();
+        log.debug("{}.{}() running time=({} ms)", className, methodName, String.format("%,d", runningTime));
+
+        return returnValue;
+    }
+
+}
