d12b22ef12965c862fa8ba1a5e9aefc3834917ab
Chiyoung Won <wcyoung98@gmail.com>
2020-02-05 00:03:33 +0900

    Update Generic

--- src/main/java/wcyoung/spring/mvc/web/version/VersionController.java
+++ src/main/java/wcyoung/spring/mvc/web/version/VersionController.java
@@ -29,10 +29,10 @@ public class VersionController extends BaseController {
             body.put("version", versionService.getManifestVersion());
         } catch (Exception e) {
             log.error("Exception: {}", ExceptionUtils.getStackTrace(e));
-            return new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return new ResponseEntity<Map<String, Object>>(body, HttpStatus.OK);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
     @GetMapping(value = "/db")
@@ -40,7 +40,7 @@ public class VersionController extends BaseController {
         Map<String, Object> body = new HashMap<>();
         body.put("version", versionService.selectDbVersion());
 
-        return new ResponseEntity<Map<String, Object>>(body, HttpStatus.OK);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
 }
