d464a7c8816309155ba2fec5915b906e629a7f34
Chiyoung Won <wcyoung98@gmail.com>
2020-03-14 08:50:14 +0900

    ResponseXssFilterAdvice filter 기능 구현

--- src/main/java/wcyoung/spring/mvc/security/ResponseXssFilterAdvice.java
+++ src/main/java/wcyoung/spring/mvc/security/ResponseXssFilterAdvice.java
@@ -1,5 +1,9 @@
 package wcyoung.spring.mvc.security;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -29,14 +33,64 @@ public class ResponseXssFilterAdvice implements ResponseBodyAdvice<Object> {
         return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType,
             MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
             ServerHttpRequest request, ServerHttpResponse response) {
 
-        // TODO value filter
+        if (body instanceof String) {
+            return filter((String) body);
+        }
+
+        if (body instanceof Map) {
+            return filter((Map<String, Object>) body);
+        }
+
+        if (body instanceof List) {
+            return filter((List<Object>) body);
+        }
 
         return body;
+    }
+
+    private String filter(String value) {
+        return value.replace("<", "&lt;").replace(">", "&gt;");
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> filter(Map<String, Object> map) {
+        for (Entry<String, Object> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            if (value instanceof String) {
+                map.put(key, filter((String) value));
+            } else if (value instanceof Map) {
+                map.put(key, filter((Map<String, Object>) value));
+            } else if (value instanceof List) {
+                map.put(key, filter((List<Object>) value));
+            }
+        }
+
+        return map;
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Object> filter(List<Object> list) {
+        for (int i = 0, length = list.size(); i < length; i++) {
+            Object value = list.get(i);
+
+            if (value instanceof String) {
+                list.set(i, filter((String) value));
+            } else if (value instanceof Map) {
+                list.set(i, filter((Map<String, Object>) value));
+            } else if (value instanceof List) {
+                list.set(i, filter((List<Object>) value));
+            }
+        }
+
+        return list;
     }
 
 }
